70d9b9d4870f70058acadce202e5bcf67497a812
amlimjoc <amlimjoc@uci.edu>
2025-12-09 19:02:44 -0800

    Adjusted ORDER BY to prioritize mid over bmid

--- commands.py
+++ commands.py
@@ -176,7 +176,7 @@ def topNDurationConfig(**kwargs):
         FROM ModelConfigurations mc
         JOIN Configuration c ON mc.cid = c.cid
         WHERE c.client_uid = %s
-        ORDER BY mc.duration DESC
+        ORDER BY mc.duration DESC, c.cid ASC, mc.mid ASC, mc.bmid ASC
         LIMIT %s
     """
     results = execute_custom_select_multi(sql, (uid, N))
